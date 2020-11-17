c60c827e3d15acdec4f5c99f72b398d43a9adaa8
gregoire cade <gregoirecade.fr@gmail.com>
2020-11-17 14:47:45 +0000

    Adding cars to the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(email:"vincentrobert.vrbt@gmail.com", password: "password", description: "I would like to rent a car", location: "London")
+10.times do
+  Car.create!(model: "205", brand: "Peugeot", price: 10, description: "This a vintage car", title: Faker::Name.name, user: user)
+end
