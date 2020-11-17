21bd70d4f021b22787b8fb7fbc0a3764e16a5878
karolina langner <karolinalangner1@gmail.com>
2020-11-17 16:25:28 +0000

    booking controller + controller index now outside nesting

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def def new
+    @booking = Booking.new
+  end
+  
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.car = Car.find(params[:car_id])
+      if @booking.save
+        redirect_to bookings_path
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id)
+    @booking.destroy
+    redirect_to bookings_path
+  end
+  
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date )
+  end
+
+end
